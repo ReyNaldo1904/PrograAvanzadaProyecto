455eb254fd311ac010b5cdc9cfa7d43d1691d10c
Reynaldo Solano Vega <114126394+ReyNaldo1904@users.noreply.github.com>
2024-11-26 16:39:41 -0900

    Add project files.

---
+++ Proyecto/Controllers/UsuariosRegistrados.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto.Controllers
+{
+    public class UsuariosRegistrados : Controller
+    {
+        // GET: UsuariosRegistrados
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: UsuariosRegistrados/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: UsuariosRegistrados/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UsuariosRegistrados/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UsuariosRegistrados/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: UsuariosRegistrados/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UsuariosRegistrados/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: UsuariosRegistrados/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Proyecto/Startup.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Owin;
+using Proyecto.Models;
+
+[assembly: OwinStartupAttribute(typeof(Proyecto.Startup))]
+namespace Proyecto
+{
+    public partial class Startup
+
+    {
+
+        public void Configuration(IAppBuilder app)
+
+        {
+
+            ConfigureAuth(app);
+
+            InitializeRolesAndAdminUser();
+
+        }
+
+        private void InitializeRolesAndAdminUser()
+
+        {
+
+            using (var context = new ApplicationDbContext())
+
+            {
+
+                var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+
+                var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+                if (!roleManager.RoleExists("Admin"))
+
+                {
+
+                    var role = new IdentityRole("Admin");
+
+                    roleManager.Create(role);
+
+                }
+
+                if (!roleManager.RoleExists("User"))
+
+                {
+
+                    var role = new IdentityRole("User");
+
+                    roleManager.Create(role);
+
+                }
+
+                if (userManager.FindByName("admin@domain.com") == null)
+
+                {
+
+                    var AdminUser = new ApplicationUser
+
+                    {
+
+                        UserName = "admin@domain.com",
+
+                        Email = "admin@domain.com"
+
+                    };
+
+                    var adminPassword = "Admin123";
+
+                    var userCreationResult = userManager.Create(AdminUser, adminPassword);
+
+                    if (userCreationResult.Succeeded)
+                    {
+
+                        userManager.AddToRole(AdminUser.Id, "Admin");
+
+                    }
+
+                }
+
+            }
+
+        }
+
+    }
+
+}
+
+
