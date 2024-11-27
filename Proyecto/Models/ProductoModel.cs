04064bd37d3289105787c34d6bdda4dd611ebc83
Diego-Valladares <diego14vb@gmail.com>
2024-11-27 15:36:13 -0600

    Agregue historial de compra y funcion de bd

---
+++ Proyecto/Models/ProductoModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Proyecto.Models
+{
+    public class Producto
+    {
+        public int Id { get; set; } // Clave primaria
+        public string Nombre { get; set; } // Nombre del producto
+        public decimal Precio { get; set; } // Precio del producto
+        public int Inventario { get; set; } // Cantidad en inventario
+        public bool Estado { get; set; } // Activo o inactivo
+        public string Imagenes { get; set; } // Almacena rutas de imagenes separadas por comas
+        public string Reviews { get; set; } // Texto plano o JSON con las reviews
+    }
+
+}
