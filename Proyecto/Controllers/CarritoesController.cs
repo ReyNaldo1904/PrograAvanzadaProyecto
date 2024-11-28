710c296bc30c90881c35846fa458faaa005e6857
LuisDavid01 <luisdavidmirandav12@gmail.com>
2024-11-27 18:13:14 -0600

    vistas y controaldores para los modelos

---
+++ Proyecto/Controllers/CarritoesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Proyecto.Models;
+
+namespace Proyecto.Controllers
+{
+    public class CarritoesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Carritoes
+        public ActionResult Index()
+        {
+            return View(db.Carritos.ToList());
+        }
+
+        // GET: Carritoes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Carrito carrito = db.Carritos.Find(id);
+            if (carrito == null)
+            {
+                return HttpNotFound();
+            }
+            return View(carrito);
+        }
+
+        // GET: Carritoes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Carritoes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,FechaCreacion,IdUsuario")] Carrito carrito)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Carritos.Add(carrito);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(carrito);
+        }
+
+        // GET: Carritoes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Carrito carrito = db.Carritos.Find(id);
+            if (carrito == null)
+            {
+                return HttpNotFound();
+            }
+            return View(carrito);
+        }
+
+        // POST: Carritoes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,FechaCreacion,IdUsuario")] Carrito carrito)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(carrito).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(carrito);
+        }
+
+        // GET: Carritoes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Carrito carrito = db.Carritos.Find(id);
+            if (carrito == null)
+            {
+                return HttpNotFound();
+            }
+            return View(carrito);
+        }
+
+        // POST: Carritoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Carrito carrito = db.Carritos.Find(id);
+            db.Carritos.Remove(carrito);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Proyecto/Controllers/PedidoController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Proyecto.Models;
+
+namespace Proyecto.Controllers
+{
+    public class PedidoController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Pedido
+        public ActionResult Index()
+        {
+            var pedidos = db.Pedidos.Include(p => p.Carrito).Include(p => p.Producto);
+            return View(pedidos.ToList());
+        }
+
+        // GET: Pedido/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pedido pedido = db.Pedidos.Find(id);
+            if (pedido == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pedido);
+        }
+
+        // GET: Pedido/Create
+        public ActionResult Create()
+        {
+            ViewBag.CarritoId = new SelectList(db.Carritos, "Id", "IdUsuario");
+            ViewBag.ProductoId = new SelectList(db.Productos, "Id", "Nombre");
+            return View();
+        }
+
+        // POST: Pedido/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,CarritoId,ProductoId,Cantidad")] Pedido pedido)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Pedidos.Add(pedido);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CarritoId = new SelectList(db.Carritos, "Id", "IdUsuario", pedido.CarritoId);
+            ViewBag.ProductoId = new SelectList(db.Productos, "Id", "Nombre", pedido.ProductoId);
+            return View(pedido);
+        }
+
+        // GET: Pedido/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pedido pedido = db.Pedidos.Find(id);
+            if (pedido == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CarritoId = new SelectList(db.Carritos, "Id", "IdUsuario", pedido.CarritoId);
+            ViewBag.ProductoId = new SelectList(db.Productos, "Id", "Nombre", pedido.ProductoId);
+            return View(pedido);
+        }
+
+        // POST: Pedido/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,CarritoId,ProductoId,Cantidad")] Pedido pedido)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(pedido).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CarritoId = new SelectList(db.Carritos, "Id", "IdUsuario", pedido.CarritoId);
+            ViewBag.ProductoId = new SelectList(db.Productos, "Id", "Nombre", pedido.ProductoId);
+            return View(pedido);
+        }
+
+        // GET: Pedido/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pedido pedido = db.Pedidos.Find(id);
+            if (pedido == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pedido);
+        }
+
+        // POST: Pedido/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Pedido pedido = db.Pedidos.Find(id);
+            db.Pedidos.Remove(pedido);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
